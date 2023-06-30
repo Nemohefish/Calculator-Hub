b3f8fbe24a1db9787a52863d884dac447c87d77d
Nemohefish <64276029+Nemohefish@users.noreply.github.com>
2023-06-30 21:34:09 +0400

    Update CH.lua

--- CH.lua
+++ CH.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 6152116144 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Nemohefish/Calculator-Hub/main/Main.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Nemohefish/Snowy-Hub/blob/main/Snowy(M1).lua"))()
 end
